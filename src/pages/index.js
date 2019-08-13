c0010cf28a8fc4373ee77321a51fcb6d78118db7
Rowan-James Tran <rowanjames.tran@gmail.com>
2019-08-13 13:04:13 -0700

    Scrolling arrow

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import React from "react"
 import {Link, graphql} from "gatsby"
 import Img from "gatsby-image"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
+
 import Layout from "../components/layout"
 import Project from "../components/project"
 import SEO from "../components/seo"
@@ -25,8 +28,13 @@ export default ({data}) => {
                         <h1 className="about-me-header">Rowan-James Tran</h1>
                         <div className="about-me-text">full-stack developer</div>
                     </div>
+                    <a className="about-me__scroll-down" href="#projects">
+                        <div className="about-me__scroll-down__icon">
+                            <FontAwesomeIcon icon={faAngleDoubleDown} />
+                        </div>
+                    </a>
                 </div>
-                <div className="projects">
+                <div className="projects" id="projects">
                     {data.allProjectsJson.edges.map(({node}) => (
                         <Project projectTitle={node.title} imageSrc={node.image.childImageSharp.fluid} imageAlt={node.description} />
                     ))}
