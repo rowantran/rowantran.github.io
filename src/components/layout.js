bcae0d805271bee2bd9e2679ff16605c3246c5ff
Rowan-James Tran <rowanjames.tran@gmail.com>
2020-07-28 21:36:14 -0700

    Add favicon and CNAME

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,29 +2,38 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
+import Helmet from "react-helmet"
+
 import Header from "./header"
 import "./layout.css"
 
+import favicon from "../images/favicon.ico"
+
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+  <>
+    <Helmet>
+      <link rel="icon" href={favicon} />
+    </Helmet>
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={() => (
-      <>
-        <main>{children}</main>
-        <footer style={{textAlign: `center`}}>
-            © {new Date().getFullYear()} Rowan-James Tran
-        </footer>
-      </>
-    )}
-  />
+      `}
+      render={() => (
+        <>
+          <main>{children}</main>
+          <footer style={{textAlign: `center`}}>
+              © {new Date().getFullYear()} Rowan-James Tran
+          </footer>
+        </>
+      )}
+    />
+  </>
 )
 
 Layout.propTypes = {
